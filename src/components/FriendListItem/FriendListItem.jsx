58953185c5cbddafe5c105b5336680fb84a79153
Maryna <yasinskamarina@gmail.com>
2023-02-07 20:01:05 +0200

    +

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+export function FriendListItem({ avatar, name, isOnline }) {
+
+  return (
+    <li className={css.item}>
+      <span className={isOnline ? css.online: css.offline}>{isOnline}</span>
+      <img class="avatar" src={avatar} alt="User avatar" width="48" />
+      <p class="name">{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+export function Statistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li className={css.item} key={id}>
+              <span className="label">{label}</span>
+              <span className="percentage">{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
